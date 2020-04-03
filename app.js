70449342d8b03c7b2892c6ab5859e0cba1cd3e1c
DakshalP <dakshal29@gmail.com>
2020-04-03 16:58:04 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var socket = require('socket.io')
+
+// App setup
+var app = express();
+
+app.use(express.static('public'))
+
+app.get('/', function(req,res) {
+    res.sendFile('/index.html');
+})
+
+app.get('/board', function(req,res) {
+    res.sendFile(__dirname + '/public/board.html');
+})
+
+var server = app.listen(4000, () => {
+    console.log('App listening on port ' + server.address().port)
+})
+
+
+function searchId(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].id === nameKey) {
+            return i;
+        }
+    }
+}
+
+//random word id generation, each role is put into roleArr when generateRoleArr() is called
+let roleArr = []
+let lengthOfBoard = 4; //only length so far
+
+function generateRoleArr(numPlayers) {
+    roleArr = ["?"]
+    let wordCode = randLetters(1) + (Math.floor(Math.random() * lengthOfBoard) + 1).toString();
+    //-1 since the unknown word is already in the array "?"  
+    for(i=0;i<numPlayers-1;i++) {
+        roleArr.push(wordCode)
+    }
+    shuffleArr(roleArr);
+}
+
+function shuffleArr(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function randLetters(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = lengthOfBoard;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+
+
+//Socket setup
+var io = socket(server);
+var connectedArr = [];
+var previousBoard;
+
+io.on('connection', (socket)=>{
+    console.log('made socket connection: ', socket.id);
+    socket.emit('initializeLobby', connectedArr);
+    if(previousBoard != null) socket.emit('changeBoard', previousBoard);
+
+    //handle events
+    socket.on('addPerson', (name) => {
+        connectedArr.push(
+            {
+                name: name,
+                id: socket.id
+            }
+        )
+        console.log(connectedArr)
+        io.sockets.emit('addPerson', name);
+    });
+
+    socket.on('giveWord', ()=>{
+        generateRoleArr(connectedArr.length);
+        for(let i=0;i<connectedArr.length;i++) {  
+            io.to(`${connectedArr[i].id}`).emit('giveWord', roleArr[i])
+        }
+    })
+
+    socket.on('disconnect', ()=> {
+        console.log('Got disconnect!: ', socket.id);
+  
+        let id = searchId(socket.id, connectedArr);
+        if(typeof id !== "undefined") {
+            console.log("REMOVE : " ,connectedArr[id].name);
+            io.sockets.emit('removePerson', connectedArr[id]);
+            connectedArr.splice(id, 1);
+        }
+    });
+
+    socket.on('changeBoard', (boardWords)=>{
+        previousBoard = boardWords;
+        io.sockets.emit('changeBoard', boardWords);
+    })
+})
---
+++ public/main.js
@@ -0,0 +1,168 @@
+//Make connection
+var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+const join = document.getElementById('join'),
+    namePrompt = document.getElementById('name'),
+    people = document.getElementById('people'),
+    output = document.getElementById('output'),
+    input = document.getElementById('input'),
+    controls = document.getElementById('controls'),
+    tagline = document.getElementById('tagline'),
+    boardButton = document.getElementById('boardButton'),
+    board = document.getElementById('board'),
+    give = document.getElementById('give'),
+    clue = document.getElementById('clue');
+
+var peopleArr = [];
+var numWords;
+var clueNum = 0;
+
+//quick title case
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+function updateOutput() {
+    console.log(peopleArr);
+    output.innerHTML = "";
+    peopleArr.forEach(person => {
+        output.innerHTML += `<p>${person}</p>`;
+    });
+}
+
+//DOM events
+join.addEventListener('click', ()=> {
+    if(namePrompt.value.trim() != "") {
+        socket.emit('addPerson', toTitleCase(namePrompt.value));
+        namePrompt.value = "";
+
+        tagline.innerText="You're in! Do you see your name?"
+        input.innerHTML="";
+        controls.style.display = "block";
+        people.style.height = "250px";
+    }
+})
+
+give.addEventListener('click', ()=>{
+    socket.emit('giveWord');
+})
+
+boardButton.addEventListener('click', ()=>{
+    if(confirm("Are you sure you want to change the board?")) {
+        var boardWords = prompt("Enter 16 words seperated by a comma.");
+        
+        //this version allows for any number of words on board
+        // if(boardWords.trim() != "") {   
+        //     socket.emit('changeBoard', boardWords);
+        // }
+
+        //check if enough commas for 16 words
+        if([15,16].includes(boardWords.split(',').length-1)) {
+            socket.emit('changeBoard', boardWords);
+        } else {
+            alert('Sorry, 16 words seperated by a comma each were not detected');
+        }
+    }
+})
+
+
+//Listen for socket events
+socket.on('initializeLobby', (arr) =>{
+    arr.forEach(person => {
+        peopleArr.push(person.name);
+    });
+    updateOutput();
+})
+socket.on('addPerson', (person)=>{
+    peopleArr.push(person);
+    updateOutput();
+    //scroll down
+    people.scrollTop = people.scrollHeight;
+})
+socket.on('removePerson', (person) => {
+    console.log(person);
+    peopleArr.splice(peopleArr.indexOf(person.name), 1)
+    updateOutput();
+})
+socket.on('giveWord', (word) =>{
+    clueNum++;
+    clue.innerHTML = (word === "?") ? `(Clue ${clueNum}) You're the one who <strong>doesn't know the word</strong>... don't tell anyone.` : `(Clue ${clueNum}) The word is at <strong>${word}</strong>`;
+    toggleModal();
+})
+socket.on('changeBoard', (boardWords)=>{
+    let words = boardWords.split(',')
+    numWords = words.length;
+
+    //group in groups of 4
+    let groupWords = []
+    while(words.length) {
+        groupWords.push(words.splice(0,4));
+    }
+
+    //add <td> tags and push back into words
+    words = []
+    groupWords.forEach((arr)=> {
+        words.push(arr.map(x => `<td>${x.trim()}</td>`))
+    })
+
+    //edit button
+    boardButton.innerText = "Change board";
+
+    //fill in to table
+    board.innerHTML = 
+    `
+        <table>
+            <tr>
+                <th></th>
+                <th>A</th>
+                <th>B</th>
+                <th>C</th>
+                <th>D</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                ${(words[0]) ? words[0].toString().replace(/,/g,'\n') : ""}
+            </tr>
+            <tr>
+                <th>2</th>
+                ${(words[1]) ? words[1].toString().replace(/,/g,'\n') : ""}
+            </tr>
+            <tr>
+                <th>3</th>
+                ${(words[2]) ? words[2].toString().replace(/,/g,'\n') : ""}
+            </tr>
+            <tr>
+                <th>4</th>
+                ${(words[3]) ? words[3].toString().replace(/,/g,'\n') : ""}
+            </tr>
+        </table>
+    `  
+})
+
+/* Modal */
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector("#trigger");
+const closeButton = document.querySelector(".close-button");
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+//random 6 digit hex id
+// function randomId() {
+//     return (Math.random()*0xFFFFFF<<0).toString(16);
+// }
+
